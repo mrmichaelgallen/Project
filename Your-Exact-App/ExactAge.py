ec45451e6812e085299c927d9a81a87a43c2c684
Michael Allen <mr.michael.g.allen@gmail.com>
2016-03-03 18:17:42 -0800

    Create ExactAge.py

---
+++ Your-Exact-App/ExactAge.py
@@ -0,0 +1,9 @@
+# The challenge with this exercise/program is that it is not exact, because it should be using your birthdate - current date to determine the exact days, minutes and seconds you lived
+
+import datetime
+print("Let's see how long you have in days, minutes and seconds.")
+name = input("What is your name: ")
+birthdate = date.year(input("What is your birth date? Example 1900,01,01: "))
+today = datetime.today()
+daysAlive = timedelta.days
+print(daysAlive)
